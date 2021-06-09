838aee3b8ebb56f185e0dafa8328f62978232fec
mkroetzsch <accounts@korrekt.org>
2021-06-09 22:41:39 +0200

    remove redundant self::

--- src/physical/datatypes.rs
+++ src/physical/datatypes.rs
@@ -1,14 +1,14 @@
 //! This module collects functionality specific to the supported primitive datatypes.
 
-/// Module for defining [`self::DataTypeName`]
+/// Module for defining [`DataTypeName`]
 pub mod data_type_name;
 pub use data_type_name::DataTypeName;
-/// Module for defining [`self::Double`]
+/// Module for defining [`Double`]
 pub mod double;
 pub use double::Double;
-/// Module for defining [`self::Float`]
+/// Module for defining [`Float`]
 pub mod float;
 pub use float::Float;
-/// Module for defining [`self::FloatIsNaN`]
+/// Module for defining [`FloatIsNaN`]
 pub mod float_is_nan;
 pub use float_is_nan::FloatIsNaN;
