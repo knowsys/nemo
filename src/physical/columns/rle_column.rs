05d2a04717067c6df517592a3b2daf5a54ed3c0c
monsterkrampe <monsterkrampe@users.noreply.github.com>
2021-11-23 17:50:48 +0100

    add basic tests for RLE column impl

--- src/physical/columns/rle_column.rs
+++ src/physical/columns/rle_column.rs
@@ -1,45 +1,57 @@
-use super::{Column,ColumnScan};
-use std::fmt::Debug;
+use super::{Column, ColumnScan};
+use std::{
+    fmt::Debug,
+    num::NonZeroUsize,
+    ops::{Add, Mul},
+};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct RleElement<T> {
     value: T,
-    length: usize,
-    increment: i32,
+    length: NonZeroUsize,
+    increment: T,
 }
 
-
-//impl<T: std::ops::Add<i64, Output=T> + Debug + Copy> Index<usize> for RleElement<T> {
-    //type Output = T;
-
-    //fn index(&self, index: usize) -> &Self::Output {
-        //&(self.value + i64::try_from(index).unwrap() * i64::from(self.increment))
-    //}
-//}
-
-impl<T: std::ops::Add<i64, Output=T> + Debug + Copy> RleElement<T> {
+impl<T: Add<Output = T> + Mul<Output = T> + TryFrom<usize> + Debug + Copy> RleElement<T> {
     fn get(&self, index: usize) -> T {
-        self.value + i64::try_from(index).unwrap() * i64::from(self.increment)
+        self.value
+            + self.increment
+                * T::try_from(index)
+                    .or(Err(
+                        "should not happen if construction of RleColumn works correctly",
+                    ))
+                    .unwrap()
     }
 }
 
 /// Implementation of [`Column`] that allows the use of incremental run length encoding.
-#[derive(Debug)]
-pub struct RleColumn<T: std::ops::Add<i64, Output=T> + Ord + Copy> {
-    elements: Vec<RleElement<T>>
+#[derive(Debug, PartialEq)]
+pub struct RleColumn<T> {
+    elements: Vec<RleElement<T>>,
 }
 
-impl<T: std::ops::Add<i64, Output=T> + Debug + Ord + Copy> RleColumn<T> {
+impl<T: Add<Output = T> + Debug + Ord + Copy + Default> RleColumn<T> {
     /// Constructs a new RleColumn from a vector of the suitable type.
     pub fn new(data: Vec<T>) -> RleColumn<T> {
         // TODO: actually use RLE
-        RleColumn { elements: data.iter().map(|e| RleElement { value: *e, length: 1, increment: 0 }).collect() }
+        RleColumn {
+            elements: data
+                .iter()
+                .map(|e| RleElement {
+                    value: *e,
+                    length: NonZeroUsize::new(1).unwrap(),
+                    increment: Default::default(),
+                })
+                .collect(),
+        }
     }
 }
 
-impl<T: std::ops::Add<i64, Output=T> + Debug + Ord + Copy> Column<T> for RleColumn<T> {
+impl<T: Add<Output = T> + Mul<Output = T> + TryFrom<usize> + Debug + Ord + Copy> Column<T>
+    for RleColumn<T>
+{
     fn len(&self) -> usize {
-        self.elements.iter().map(|e| e.length).sum()
+        self.elements.iter().map(|e| e.length.get()).sum()
     }
 
     fn is_empty(&self) -> bool {
@@ -49,8 +61,8 @@ impl<T: std::ops::Add<i64, Output=T> + Debug + Ord + Copy> Column<T> for RleColu
     fn get(&self, index: usize) -> T {
         let mut target_index = index;
         let mut element_index = 0;
-        while target_index > self.elements[element_index].length {
-            target_index -= self.elements[element_index].length;
+        while target_index >= self.elements[element_index].length.get() {
+            target_index -= self.elements[element_index].length.get();
             element_index += 1;
         }
         self.elements[element_index].get(target_index)
@@ -61,26 +73,18 @@ impl<T: std::ops::Add<i64, Output=T> + Debug + Ord + Copy> Column<T> for RleColu
     }
 }
 
-//impl<T: std::ops::Add<i64, Output=T> + Debug + Ord + Copy> Index<usize> for RleColumn<T> {
-    //type Output = T;
-
-    //fn index(&self, index: usize) -> &Self::Output {
-         //&self.get(index)
-    //}
-//}
-
 #[derive(Debug)]
-struct RleColumnScan<'a, T: std::ops::Add<i64, Output=T> + Ord + Copy> {
+struct RleColumnScan<'a, T> {
     column: &'a RleColumn<T>,
     element_index: Option<usize>,
     increment_index: Option<usize>,
     current: Option<T>,
 }
 
-impl<'a, T: std::ops::Add<i64, Output=T> + Ord + Copy> RleColumnScan<'a, T> {
+impl<'a, T: Add<Output = T> + Ord + Copy> RleColumnScan<'a, T> {
     pub fn new(column: &'a RleColumn<T>) -> RleColumnScan<'a, T> {
-        RleColumnScan { 
-            column, 
+        RleColumnScan {
+            column,
             element_index: None,
             increment_index: None,
             current: None,
@@ -88,16 +92,16 @@ impl<'a, T: std::ops::Add<i64, Output=T> + Ord + Copy> RleColumnScan<'a, T> {
     }
 }
 
-impl<'a, T: std::ops::Add<i64, Output=T> + Ord + Copy> Iterator for RleColumnScan<'a, T> {
+impl<'a, T: Add<Output = T> + Ord + Copy> Iterator for RleColumnScan<'a, T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut element_index = self.element_index.unwrap_or_default();
-        let mut increment_index = self.increment_index.map_or_else(Default::default, |i| i + 1);
-        
-        // only using while here for the case that e RleElement should have length 0, which isn't
-        // intended anyway but better safe than sorry :)
-        while element_index < self.column.elements.len() && increment_index >= self.column.elements[element_index].length {
+        let mut increment_index = self
+            .increment_index
+            .map_or_else(Default::default, |i| i + 1);
+
+        if increment_index >= self.column.elements[element_index].length.get() {
             element_index += 1;
             increment_index = 0;
         }
@@ -105,21 +109,20 @@ impl<'a, T: std::ops::Add<i64, Output=T> + Ord + Copy> Iterator for RleColumnSca
         self.element_index = Some(element_index);
         self.increment_index = Some(increment_index);
 
-        self.current = (element_index < self.column.elements.len()).then(||
+        self.current = (element_index < self.column.elements.len()).then(|| {
             if increment_index == 0 {
                 self.column.elements[element_index].value
             } else {
                 // self.current should always contain a value here
-                self.current.unwrap() + self.column.elements[element_index].increment.into()
+                self.current.unwrap() + self.column.elements[element_index].increment
             }
-        );
+        });
 
         self.current
     }
 }
 
-impl<'a, T: std::ops::Add<i64, Output=T> + Debug + Ord + Copy> ColumnScan for RleColumnScan<'a, T> {
-
+impl<'a, T: Add<Output = T> + Debug + Ord + Copy> ColumnScan for RleColumnScan<'a, T> {
     /// Find the next value that is at least as large as the given value,
     /// advance the iterator to this position, and return the value.
     fn seek(&mut self, value: Self::Item) -> Option<Self::Item> {
@@ -127,7 +130,7 @@ impl<'a, T: std::ops::Add<i64, Output=T> + Debug + Ord + Copy> ColumnScan for Rl
         // for now we don't
         for next in self {
             if next >= value {
-                return Some(next)
+                return Some(next);
             }
         }
         None
@@ -138,3 +141,86 @@ impl<'a, T: std::ops::Add<i64, Output=T> + Debug + Ord + Copy> ColumnScan for Rl
     }
 }
 
+#[cfg(test)]
+mod test {
+    use super::{Column, RleColumn, RleElement};
+    use std::num::NonZeroUsize;
+    use test_env_log::test;
+
+    fn get_control_data() -> Vec<u64> {
+        vec![2, 5, 6, 7, 8, 42, 4, 7, 10, 13, 16]
+    }
+
+    fn get_test_column() -> RleColumn<u64> {
+        RleColumn {
+            elements: vec![
+                RleElement {
+                    value: 2,
+                    length: NonZeroUsize::new(1).unwrap(),
+                    increment: 0,
+                },
+                RleElement {
+                    value: 5,
+                    length: NonZeroUsize::new(4).unwrap(),
+                    increment: 1,
+                },
+                RleElement {
+                    value: 42,
+                    length: NonZeroUsize::new(1).unwrap(),
+                    increment: 0,
+                },
+                RleElement {
+                    value: 4,
+                    length: NonZeroUsize::new(5).unwrap(),
+                    increment: 3,
+                },
+            ],
+        }
+    }
+
+    #[test]
+    fn construction() {
+        let raw_data = get_control_data();
+        let expected: RleColumn<u64> = get_test_column();
+
+        let constructed = RleColumn::new(raw_data);
+        assert_eq!(expected, constructed);
+    }
+
+    #[test]
+    fn is_empty() {
+        let c: RleColumn<u64> = get_test_column();
+        assert_eq!(c.is_empty(), false);
+
+        let c_empty: RleColumn<u64> = RleColumn { elements: vec![] };
+        assert_eq!(c_empty.is_empty(), true);
+    }
+
+    #[test]
+    fn len() {
+        let control_data = get_control_data();
+        let c: RleColumn<u64> = get_test_column();
+        assert_eq!(c.len(), control_data.len());
+    }
+
+    #[test]
+    fn get() {
+        let control_data = get_control_data();
+        let c: RleColumn<u64> = get_test_column();
+
+        for i in 0..8 {
+            assert_eq!(c.get(i), control_data[i]);
+        }
+    }
+
+    #[test]
+    fn iter() {
+        let control_data: Vec<u64> = get_control_data();
+        let c: RleColumn<u64> = get_test_column();
+
+        let iterated_c: Vec<u64> = c.iter().collect();
+        assert_eq!(iterated_c, control_data);
+    }
+
+    // TODO: tests for column scan; we should split this file up anyway...
+}
