84bdf5bd5fb19f0554920617a4f90b2f9e39c31f
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2022-03-03 11:30:45 +0100

    RESOLVE review comments
    * `VecT` moved into the datatypes module
    * Test simplified
    * quickcheck added

--- src/physical/datatypes/data_value.rs
+++ src/physical/datatypes/data_value.rs
@@ -40,3 +40,55 @@ impl DataValueT {
         }
     }
 }
+
+/// Enum for vectors of different supported input types
+#[derive(Debug)]
+pub enum VecT {
+    /// Case Vec<u64>
+    VecU64(Vec<u64>),
+    /// Case Vec<Float>
+    VecFloat(Vec<Float>),
+    /// Case Vec<Double>
+    VecDouble(Vec<Double>),
+}
+
+impl VecT {
+    /// Removes the last element in the corresponding vector
+    pub fn pop(&mut self) {
+        match self {
+            VecT::VecU64(v) => {
+                v.pop();
+            }
+            VecT::VecFloat(v) => {
+                v.pop();
+            }
+            VecT::VecDouble(v) => {
+                v.pop();
+            }
+        }
+    }
+
+    /// Inserts the Value to the corresponding Vector if the datatypes are compatible
+    /// Note that it is not checked if the [DataValueT] has the right enum-variant
+    pub(crate) fn push(&mut self, value: &DataValueT) {
+        match self {
+            VecT::VecU64(vec) => vec.push(value.as_u64().unwrap()),
+            VecT::VecFloat(vec) => vec.push(value.as_float().unwrap()),
+            VecT::VecDouble(vec) => vec.push(value.as_double().unwrap()),
+        };
+    }
+
+    /// Returns the lengths of the contained Vector
+    pub fn len(&self) -> usize {
+        match self {
+            VecT::VecU64(vec) => vec.len(),
+            VecT::VecFloat(vec) => vec.len(),
+            VecT::VecDouble(vec) => vec.len(),
+        }
+    }
+
+    /// Returns whether the vector is empty, or not
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
