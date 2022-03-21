b76e48327d5ad8d6ce94f1a0c805a50628129d03
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2022-03-21 12:40:02 +0100

    Add wrapper functions for ColumnT

--- src/physical/columns/column.rs
+++ src/physical/columns/column.rs
@@ -1,5 +1,5 @@
 use super::ColumnScan;
-use crate::physical::datatypes::{Double, Float};
+use crate::physical::datatypes::{DataValueT, Double, Float};
 use std::fmt::Debug;
 
 /// Column of ordered values.
@@ -31,4 +31,32 @@ pub enum ColumnT {
     ColumnFloat(Box<dyn Column<Float>>),
     /// Case Column<Double>
     ColumnDouble(Box<dyn Column<Double>>),
+}
+
+impl ColumnT {
+    /// Returns the number of entries in the [`Column`]
+    pub fn len(&self) -> usize {
+        match self {
+            ColumnT::ColumnU64(col) => col.len(),
+            ColumnT::ColumnFloat(col) => col.len(),
+            ColumnT::ColumnDouble(col) => col.len(),
+        }
+    }
+
+    /// Returns the value, wrapped in a [`DataValueT`][crate::physical::datatypes::DataValueT], at a given index.
+    ///
+    /// # Panics
+    /// Panics if `index` is out of bounds.
+    pub fn get(&self, index: usize) -> DataValueT {
+        match self {
+            ColumnT::ColumnU64(col) => DataValueT::U64(col.get(index)),
+            ColumnT::ColumnFloat(col) => DataValueT::Float(col.get(index)),
+            ColumnT::ColumnDouble(col) => DataValueT::Double(col.get(index)),
+        }
+    }
+
+    /// Returns [`true`] iff the column is empty
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
 }
