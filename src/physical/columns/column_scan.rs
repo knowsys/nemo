b5b90b024b64f521d9e7b0c3e6d790783367f2f6
Alex Ivliev <alex.ivliev@tu-dresden.de>
2022-07-14 13:03:40 +0200

    Add comment explaining the purpose of the reset function

--- src/physical/columns/column_scan.rs
+++ src/physical/columns/column_scan.rs
@@ -11,5 +11,12 @@ pub trait ColumnScan: Debug + Iterator {
     fn current(&mut self) -> Option<Self::Item>;
 
     /// Return to the initial state
+    /// Typically, the state of a [`ColumnScan`] is determined by the state of its sub scans
+    /// as well as some additional information.
+    /// This function is only supposed to reset the latter to its initial state (i.e. after calling new).
+    /// The intention of this function is to use it after the internal iterators have been reset from the outside
+    /// (e.g. by calling down() on the TrieScan that owns the sub iterators).
+    /// So this call should be present in every implementation of the down() method of a TrieScan.
+    /// TODO: Think about ways to avoid this
     fn reset(&mut self);
 }
