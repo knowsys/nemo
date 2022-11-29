220d56925f7b54c2e315ab25339511e3e05d8cd3
Alex Ivliev <alex.ivliev@tu-dresden.de>
2022-11-29 10:35:09 +0100

    Renamed columns and tables to columnar and tabular

--- src/physical/columnar.rs
+++ src/physical/columnar.rs
@@ -1,8 +1,14 @@
 //! This module collects data structures and operations on individual columns.
 
+<<<<<<< HEAD
 /// Module for defining [`ColumnBuilderAdaptive`] and [`ColumnBuilderAdaptiveT`]
 pub mod adaptive_column_builder;
 
 pub mod column_types;
 pub mod operations;
 pub mod traits;
+=======
+pub mod builders;
+pub mod colscans;
+pub mod columns;
+>>>>>>> e09827b (Renamed columns and tables to columnar and tabular)
