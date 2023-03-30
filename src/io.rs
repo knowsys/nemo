c8270d4afc72a2d952b7bd794addb134ef645e7f
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2023-03-30 09:53:44 +0200

    Move builder_proxy into io and rename trait methods
    Renamed trait methods to better reflect what the proxy does

--- src/io.rs
+++ src/io.rs
@@ -1,4 +1,5 @@
 //! This module contains utility functions, like importing data from various sources
 
+pub mod builder_proxy;
 pub mod csv;
 pub mod parser;
--- src/physical/columnar.rs
+++ src/physical/columnar.rs
@@ -2,8 +2,6 @@
 
 /// Module for defining [`ColumnBuilderAdaptive`] and [`ColumnBuilderAdaptiveT`]
 pub mod adaptive_column_builder;
-/// Adaptive Builder to create [`VecT`] columns, based on streamed data
-pub mod proxy_builder;
 
 pub mod column_types;
 pub mod operations;
