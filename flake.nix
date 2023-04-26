b0f5447f23887e0eb0ca5f1560f22970a9962022
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-04-26 14:29:05 +0200

    Modernise flake

--- flake.nix
+++ flake.nix
@@ -1,75 +1,77 @@
-{
-  description = "nemo, the next stage of Datalog reasoning";
+rec {
+  description =
+    "nemo, a datalog-based rule engine for fast and scalable analytic data processing in memory";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs = {
+      nixpkgs.follows = "nixpkgs";
+      flake-utils.follows = "utils/flake-utils";
     };
-    flake-utils.url = "github:numtide/flake-utils";
-    gitignoresrc = {
-      url = "github:hercules-ci/gitignore.nix";
-      flake = false;
-    };
+
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }@inputs:
-    { } // (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
-        toolchain = (pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-          toolchain.default.override { extensions = [ "rust-src" "miri" ]; }));
-        platform = pkgs.makeRustPlatform {
-          cargo = toolchain;
-          rustc = toolchain;
-        };
-      in rec {
+  outputs = inputs@{ self, utils, rust-overlay, ... }:
+    utils.lib.mkFlake {
+      inherit self inputs;
 
-        packages = rec {
-          nemo = platform.buildRustPackage {
-            pname = "nemo";
-            version = "0.1.0";
-            src = ./.;
+      channels.nixpkgs.overlaysBuilder = channels:
+        [ rust-overlay.overlays.default ];
 
-            cargoLock.lockFile = ./nix/Cargo.lock;
-            postPatch = ''
-              ln -s ${./nix/Cargo.lock} Cargo.lock
-            '';
+      outputsBuilder = channels:
+        let
+          pkgs = channels.nixpkgs;
+          toolchain = (pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+            toolchain.default.override {
+              extensions = [ "rust-src" "miri" ];
+            }));
+          platform = pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+        in {
+          packages = rec {
+            nemo = platform.buildRustPackage {
+              pname = "nemo";
+              version = "0.1.0";
+              src = ./.;
 
-            meta = {
-              description = "nemo, the next stage of Datalog reasoning";
-              homepage = "htps://github.com/knowsys/nemo";
-              license = [ pkgs.lib.licenses.asl20 ];
+              cargoLock.lockFile = ./nix/Cargo.lock;
+              postPatch = ''
+                ln -s ${./nix/Cargo.lock} Cargo.lock
+              '';
+
+              meta = {
+                inherit description;
+                homepage = "htps://github.com/knowsys/nemo";
+                license = [ pkgs.lib.licenses.asl20 pkgs.lib.licenses.mit ];
+              };
             };
+
+            default = nemo;
           };
 
-          default = nemo;
+          devShells.default = pkgs.mkShell {
+            RUST_LOG = "debug";
+            RUST_BACKTRACE = 1;
+
+            shellHook = ''
+              export PATH=''${HOME}/.cargo/bin''${PATH+:''${PATH}}
+            '';
+
+            buildInputs = [
+              toolchain
+              pkgs.rust-analyzer
+              pkgs.cargo-audit
+              pkgs.cargo-license
+              pkgs.cargo-tarpaulin
+              pkgs.valgrind
+              pkgs.gnuplot
+            ];
+          };
         };
-
-        devShells.default = pkgs.mkShell {
-          RUST_LOG = "debug";
-          RUST_BACKTRACE = 1;
-
-          shellHook = ''
-            export PATH=''${HOME}/.cargo/bin''${PATH+:''${PATH}}
-          '';
-
-          buildInputs = [
-            toolchain
-            pkgs.rust-analyzer
-            pkgs.cargo-audit
-            pkgs.cargo-license
-            pkgs.cargo-tarpaulin
-            pkgs.valgrind
-            pkgs.gnuplot
-          ];
-        };
-      }));
+    };
 }
