016d630141e2b5413d1ba279774eba16bb82cfa2
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2023-05-15 14:47:00 +0200

    Add documentation physical builder proxy

--- src/physical/builder_proxy.rs
+++ src/physical/builder_proxy.rs
@@ -1,4 +1,4 @@
-//! The physical builder proxy takes values of some input type T and provides functionality to store them in a ['VecT']
+//! The physical builder proxy takes values of some input type `T` and provides functionality to store them in a ['VecT']
 use std::cell::RefCell;
 
 use crate::error::Error;
@@ -9,13 +9,21 @@ use crate::physical::{
     management::database::Dict,
 };
 
-/// Trait for a Builder Proxy, which translates a value for a particular [`DataTypeName`] to the corresponding [`StorageType`][crate::physical::datatypes::StorageType] Column elements
+/// Trait for a Builder Proxy, which translates a value for a particular `DataValue` to the corresponding [`StorageType`][crate::physical::datatypes::StorageTypeName] eolumn elements
+///
+/// The intended logic for this trait is as follows:
+/// - [`add`][Self::add]: caches a value to be added to the `StorageType`. If a value is already in the cache, the previously cached value will be [`commited`][Self::commit].
+/// - [`forget`][Self::forget]: forgets the cached value if one exists.
+/// - [`commit`][Self::commit]: takes the value which is cached and stores it in the column.
 pub trait ColumnBuilderProxy<T>: std::fmt::Debug {
-    /// Cache a value to be added to the ColumnBuilder. If a value is already cached, this one is actually added to the ColumnBuilder before the new value is checked
+    /// Cache a value to be added to the ColumnBuilder. If a value is already cached, the cached value will be added to the ColumnBuilder before the new value is checked.
+    ///
+    /// The add function does check if the given input can be cast or parsed into the ColumnBuilder type.
+    /// If this is not possible a corresponding [`Error`] is returned.
     fn add(&mut self, input: T) -> Result<(), Error>;
-    /// Forgets a cached value
+    /// Forgets a cached value.
     fn forget(&mut self);
-    /// Commits the data, cleaning the cached value while adding it to the respective ColumnBuilder
+    /// Commits the data, cleaning the cached value while adding it to the respective ColumnBuilder.
     fn commit(&mut self);
 }
 
@@ -55,8 +63,16 @@ macro_rules! physical_generic_trait_impl {
 }
 
 /// Trait for Builder Proxy for the physical layer
+///
+/// This trait allows to get a [`VecT`] representation of a [`PhysicalColumnBuilderProxy`].
 pub trait PhysicalColumnBuilderProxy<T>: ColumnBuilderProxy<T> {
-    /// Writes the remaining prepared value and returns a VecT
+    /**
+    Writes the remaining prepared value and returns a VecT
+
+    This will call [`commit`][ColumnBuilderProxy::commit] of the [`ColumnBuilderProxy`].
+    # Note
+    Rollbacks, if necessary, need to be done before this method is called.
+    */
     fn finalize(self) -> VecT;
 }
 
