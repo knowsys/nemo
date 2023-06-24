58a03d9e27900950e918471f4cb554400ce9d192
Matthias Meissner <geige.matze@gmail.com>
2023-06-25 00:09:06 +0200

    implement end to end blackbox tests in python

--- nemo-python/tests/unitest.py
+++ nemo-python/tests/unitest.py
@@ -1,5 +1,10 @@
 import unittest
-from nmo_python import load_string, NemoEngine, NemoOutputManager
+import csv
+import os
+import math
+import sys
+
+from nmo_python import load_string, load_file, NemoEngine, NemoOutputManager
 
 class TestStringMethods(unittest.TestCase):
     def test_reason(self):
@@ -23,5 +28,52 @@ class TestStringMethods(unittest.TestCase):
 
         self.assertEqual(result, expected_result)
 
-if __name__ == '__main__':
-    unittest.main()
+def stringify(value):
+    if not type(value) is float:
+        return str(value)
+
+    if math.floor(value) == value:
+        return str(math.floor(value))
+
+    return str(value)
+
+class TestEndToEnd(unittest.TestCase):
+    def __init__(self, path, *args, **kwargs):
+        super(TestEndToEnd, self).__init__(*args, **kwargs)
+        self.path = path
+
+    def test_end_to_end(self):
+        os.chdir(self.path)
+        for file in os.listdir('.'):
+            if not os.path.isfile(file) or not file.endswith('.rls'):
+                continue
+
+            program_name = file.removesuffix('.rls')
+            program = load_file(file)
+            engine = NemoEngine(program)
+            engine.reason()
+
+            for relation in program.output_predicates():
+                results_file_name = f"{program_name}/{relation}.csv"
+                if not os.path.exists(results_file_name):
+                    continue
+
+                with open(results_file_name) as results_file:
+                    expected = list(csv.reader(results_file))
+
+                    for result in engine.result(relation):
+                        result = [stringify(v) for v in result]
+
+                        self.assertIn(result, expected, f"error at {self.path}/{relation}")
+
+suite = unittest.TestSuite()
+suite.addTest(TestStringMethods('test_reason'))
+
+test_cases = sys.argv[-1]
+
+for directory in os.listdir(test_cases):
+    path = os.path.abspath(os.path.join(test_cases, directory))
+    suite.addTest(TestEndToEnd(path, 'test_end_to_end'))
+
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(suite)
