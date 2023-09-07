8279ff7b37745fe257292615cc17c852a384b396
Larry Gonzalez <larry.gonzalez@tu-dresden.de>
2023-09-07 16:11:41 +0200

    format

--- nemo-benches/src/bin/dict-bench.rs
+++ nemo-benches/src/bin/dict-bench.rs
@@ -1,54 +1,59 @@
+use flate2::read::MultiGzDecoder;
 use std::env;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
 use std::io::stdin;
-use flate2::read::MultiGzDecoder;
+use std::io::BufReader;
 
 use nemo::meta::{timing::TimedDisplay, TimedCode};
-use nemo_physical::dictionary::{Dictionary,AddResult,
-    string_dictionary::StringDictionary,
-    prefixed_string_dictionary::PrefixedStringDictionary,
-    hash_map_dictionary::HashMapDictionary,
-    meta_dictionary::MetaDictionary};
+use nemo_physical::dictionary::{
+    hash_map_dictionary::HashMapDictionary, meta_dictionary::MetaDictionary,
+    prefixed_string_dictionary::PrefixedStringDictionary, string_dictionary::StringDictionary,
+    AddResult, Dictionary,
+};
 
 fn create_dictionary(dict_type: &str) -> Box<dyn Dictionary> {
     match dict_type {
         "hash" => {
             println!("Using StringDictionary.");
             Box::new(StringDictionary::new())
-        },
+        }
         "hashmap" => {
             println!("Using HashMapDictionary.");
             Box::new(HashMapDictionary::new())
-        },
+        }
         "prefix" => {
             println!("Using PrefixedStringDictionary.");
             Box::new(PrefixedStringDictionary::new())
-        },
+        }
         "meta" => {
             println!("Using MetaDictionary.");
             Box::new(MetaDictionary::new())
-        },
+        }
         _ => panic!("Unexpected dictionary type '{}'.", dict_type),
     }
 }
-
 
 fn main() {
     TimedCode::instance().start();
 
     let args: Vec<_> = env::args().collect();
-    if args.len() <3 {
+    if args.len() < 3 {
         println!("Usage: dict-bench <filename> <dicttype>");
-        println!("  <filename> File with dictionary entries, one per line, possibly with duplicates.");
-        println!("  <dicttype> Identifier for the dictionary to test, e.g., \"hash\" or \"prefix\".");
+        println!(
+            "  <filename> File with dictionary entries, one per line, possibly with duplicates."
+        );
+        println!(
+            "  <dicttype> Identifier for the dictionary to test, e.g., \"hash\" or \"prefix\"."
+        );
     }
 
     let filename = &args[1];
     let dicttype = &args[2];
 
-    let reader = BufReader::new(MultiGzDecoder::new(File::open(filename).expect("Cannot open file.")));
+    let reader = BufReader::new(MultiGzDecoder::new(
+        File::open(filename).expect("Cannot open file."),
+    ));
 
     let mut dict = create_dictionary(dicttype);
     let mut count_lines = 0;
@@ -59,14 +64,17 @@ fn main() {
 
     println!("Starting to fill dictionary ...");
 
-    for l in reader.lines()  {
+    for l in reader.lines() {
         let s = l.unwrap();
         let b = s.len();
 
         let entry_status = dict.add_string(s);
         match entry_status {
-            AddResult::Fresh(_value) => {bytes = bytes + b; count_unique += 1; },
-            AddResult::Known(_value) => {},
+            AddResult::Fresh(_value) => {
+                bytes = bytes + b;
+                count_unique += 1;
+            }
+            AddResult::Known(_value) => {}
             AddResult::Rejected => {}
         }
 
@@ -77,9 +85,7 @@ fn main() {
 
     println!(
         "Processed {} strings (dictionary containts {} unique strings with {} bytes overall).",
-        count_lines,
-        count_unique,
-        bytes
+        count_lines, count_unique, bytes
     );
 
     TimedCode::instance().stop();
@@ -97,10 +103,11 @@ fn main() {
     );
 
     println!("All done. Press return to end benchmark (and free all memory).");
-    let mut s=String::new();
+    let mut s = String::new();
     stdin().read_line(&mut s).expect("No string entered?");
-    
-    if dict.len() == 123456789 { // FWIW, prevent dict from going out of scope before really finishing
+
+    if dict.len() == 123456789 {
+        // FWIW, prevent dict from going out of scope before really finishing
         println!("Today is your lucky day.");
     }
 }
