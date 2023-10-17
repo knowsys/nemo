c08d5fe9d570dac9a58a0927e2d7d939f5722608
Alex Ivliev <alex.ivliev@tu-dresden.de>
2023-10-17 09:53:01 +0200

    Change display of numeric literal

--- nemo/src/model/rule_model/numeric_literal.rs
+++ nemo/src/model/rule_model/numeric_literal.rs
@@ -1,7 +1,7 @@
 use nemo_physical::datatypes::Double;
 
 /// A numerical literal.
-#[derive(Debug, Eq, PartialEq, Copy, Clone, PartialOrd, Ord)]
+#[derive(Eq, PartialEq, Copy, Clone, PartialOrd, Ord)]
 pub enum NumericLiteral {
     /// An integer literal.
     Integer(i64),
@@ -11,12 +11,24 @@ pub enum NumericLiteral {
     Double(Double),
 }
 
-impl std::fmt::Display for NumericLiteral {
+impl std::fmt::Debug for NumericLiteral {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             NumericLiteral::Integer(value) => write!(f, "{value}"),
             NumericLiteral::Decimal(left, right) => write!(f, "{left}.{right}"),
             NumericLiteral::Double(value) => write!(f, "{:E}", f64::from(*value)),
+        }
+    }
+}
+
+impl std::fmt::Display for NumericLiteral {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            NumericLiteral::Integer(value) => write!(f, "{value}"),
+            NumericLiteral::Decimal(left, right) => write!(f, "{left}.{right}"),
+            NumericLiteral::Double(value) => {
+                f.write_str(format!("{:.4}", f64::from(*value)).trim_end_matches(['.', '0']))
+            }
         }
     }
 }
