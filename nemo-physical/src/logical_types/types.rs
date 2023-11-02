770ea78bf4dacd7a1ebd5b935704229a543826a9
Markus Krötzsch <markus.kroetzsch@tu-dresden.de>
2023-11-02 16:50:33 +0100

    updated data model playground

--- nemo-physical/src/logical_types/types.rs
+++ nemo-physical/src/logical_types/types.rs
@@ -2,121 +2,155 @@ use std::fmt::write;
 
 use crate::error::Error;
 
-pub trait DataWriter {
-    /// Output a string
-    fn write_string(&self, string: &str) -> Result<(), Error>;
-
-    /// Output an integer
-    /// TODO: Should cover a broader range of integer types than u64
-    fn write_integer(&self, integer: u64) -> Result<(), Error>;
-}
-
-/// https://www.w3.org/TR/rdf11-concepts/#xsd-datatypes
+/// Enum of different value domains that are distinguished in this code,
+/// such as "string" or "64bit floating point numbers". Even in an untyped context,
+/// it is often useful to distinguish some basic domains and to treat values accordingly.
+/// 
+/// Note: This is meant as an example now, not as a full list of everything we might need.
 #[derive(Debug, Copy, Clone)]
-pub enum DataType {
-    /// Character strings (but not all Unicode character strings)
+pub enum ValueDomain {
+    /// Domain of all strings of Unicode glyphs.
     String,
-    /// true, false
-    Boolean,
-    /// Arbitrary-precision decimal numbers
-    Decimal,
-    /// Arbitrary-size integer numbers
-    Integer,
-    /// 64-bit floating point numbers incl. ±Inf, ±0, NaN
-    /// TODO: We do not allow NaN in our Double type
+    /// Domain of all strings of Unicode glyphs, together with additional language tags
+    /// (which are non-empty strings).
+    LanguageTaggedString,
+    /// Domain of all IRIs in canonical form (no escape characters)
+    Iri,
+    /// Domain of all 64bit floating point numbers incl. ±Inf, ±0, NaN.
+    /// This set of values is disjoint from all other numerical domains.
     Double,
-    /// 32-bit floating point numbers incl. ±Inf, ±0, NaN
-    /// TODO: We do not allow NaN in our Float type
-    Float,
-    /// 64 bit signed integer: -9223372036854775808…+9223372036854775807
+    /// Domain of all signed 64bit integer numbers: -9223372036854775808…+9223372036854775807.
+    /// This is a superset of [`ValueDomain::Int`].
     Long,
-    /// 32 bit signed integer: -2147483648…+2147483647
+    /// Domain of all signed 32bit integer numbers: -2147483648…+2147483647.
+    /// This is a subset of [`ValueDomain::Long`].
     Int,
-    // ...
+    /// Domain of all data values not covered by the remaining domains
+    Other,
 }
 
+/// Trait for a data value. Implementations of this trait define a single
+/// semantic value. It is fully identified by its datatype IRI and lexical
+/// value, and different datatype IRI -- lexical value combinations always
+/// mean different values (i.e., the representation is canonical). The possible
+/// set of datatypes is unrestricted, but values of unknown types are treated
+/// literally and cannot be canonized.
 pub trait DataValue {
-    /// Return the [`DataType`] of this value
-    fn data_type(&self) -> DataType;
+    /// Return the datatype of this value, specified by an IRI.
+    /// For example, the RDF literal `"abc"^^<http://www.w3.org/2001/XMLSchema#string>`
+    /// has datatype IRI `http://www.w3.org/2001/XMLSchema#string`, without any surrounding
+    /// brackets.
+    fn datatype_iri(&self) -> String;
 
-    ///
-    fn serialize<Writer: DataWriter>(&self, writer: &Writer) -> Result<(), Error>;
+    /// Return the canonical lexical representation of this value.
+    /// For example, the RDF literal `"42"^^<http://www.w3.org/2001/XMLSchema#int>`
+    /// has lexical value `42`, without any surrounding quotes.
+    fn lexical_value(&self) -> String;
 
-    /// Return a string representation of the value
-    /// Returns `None` if the underlying value cannot be converted to a string.
-    ///
-    /// TODO: Should this only work on the String type?
-    fn to_string(&self) -> Option<String>;
+    /// Return the most specific [`ValueDomain`] of this value.
+    fn value_domain(&self) -> ValueDomain;
 
-    /// Return the 64-bit unsigned integer that represents the value
-    /// Returns `None` if the underlying value cannot be converted to a u64.
-    fn to_u64(&self) -> Option<u64>;
+    /// Return the string that this value represents, if it is a value in
+    /// the domain [`ValueDomain::String`]. Otherwise it panics.
+    fn to_string(&self) -> String {
+        panic!("Value is not a string.");
+    }
 
-    /// Return the 32-bit unsigned integer that represents the value
-    /// Returns `None` if the underlying value cannot be converted to a u64.
-    fn to_u32(&self) -> Option<u32>;
+    /// Return the pair of string and language tag that this value represents, if it is a value in
+    /// the domain [`ValueDomain::LanguageTaggedString`]. Otherwise it panics.
+    fn to_language_tagged_string(&self) -> (String,String) {
+        panic!("Value is not a language tagged string.");
+    }
 
-    // ...
+    /// Return the string that this value represents, if it is a value in
+    /// the domain [`ValueDomain::Iri`]. Otherwise it panics.
+    fn to_iri(&self) -> String {
+        panic!("Value is not an IRI.");
+    }
+
+    /// Return the f64 that this value represents, if it is a value in
+    /// the domain [`ValueDomain::Double`]. Otherwise it panics.
+    fn to_f64(&self) -> f64 {
+        panic!("Value is not a double (64bit floating point number).");
+    }
+
+    /// Return the i64 that this value represents, if it is a value in
+    /// the domain [`ValueDomain::Long`] or a subdoman thereof. Otherwise it panics.
+    fn to_i64(&self) -> i64 {
+        panic!("Value is not a long (64bit signed integer number).");
+    }
+
+    /// Return the i32 that this value represents, if it is a value in
+    /// the domain [`ValueDomain::Int`] or a subdoman thereof. Otherwise it panics.
+    fn to_i32(&self) -> i32 {
+        panic!("Value is not an int (32bit signed integer number).");
+    }
+
 }
 
-/// String value obtianed from the physical layer
-#[derive(Debug)]
-pub struct PhysicalString<'a> {
-    // TODO: Just guessing
-    prefix: &'a str,
-    content: &'a str,
-}
 
-impl<'a> DataValue for PhysicalString<'a> {
-    fn data_type(&self) -> DataType {
-        DataType::String
-    }
+/// String value obtained from the physical layer
+// #[derive(Debug)]
+// pub struct PhysicalString<'a> {
+//     // TODO: Just guessing
+//     prefix: &'a str,
+//     content: &'a str,
+// }
 
-    fn to_string(&self) -> Option<String> {
-        let mut string = String::from(self.prefix);
-        string.push_str(self.content);
+// impl<'a> DataValue for PhysicalString<'a> {
+//     fn data_type(&self) -> ValueDomain {
+//         ValueDomain::String
+//     }
 
-        Some(string)
-    }
+//     fn to_string(&self) -> Option<String> {
+//         let mut string = String::from(self.prefix);
+//         string.push_str(self.content);
 
-    fn to_u64(&self) -> Option<u64> {
-        None
-    }
+//         Some(string)
+//     }
 
-    fn to_u32(&self) -> Option<u32> {
-        None
-    }
+//     fn to_u64(&self) -> Option<u64> {
+//         None
+//     }
 
-    fn serialize<Writer: DataWriter>(&self, writer: &Writer) -> Result<(), Error> {
-        writer.write_string(self.prefix)?;
-        writer.write_string(self.content)
-    }
-}
+//     fn to_u32(&self) -> Option<u32> {
+//         None
+//     }
+// }
 
-/// Representation of the Long data type
+/// Physical representation of an integer as an i64.
 #[derive(Debug, Clone, Copy)]
-pub struct Long(u64);
+pub struct Long(i64);
 
 impl DataValue for Long {
-    fn data_type(&self) -> DataType {
-        DataType::Long
+
+    fn datatype_iri(&self) -> String {
+        match self.value_domain() {
+            ValueDomain::Long => "http://www.w3.org/2001/XMLSchema#long".to_owned(),
+            ValueDomain::Int => "http://www.w3.org/2001/XMLSchema#int".to_owned(),
+            _ => panic!("Unexpected value domain for i64"),
+        }
     }
 
-    fn to_string(&self) -> Option<String> {
-        // TODO: Should this the value as a string instead?
-        None
+    fn lexical_value(&self) -> String {
+        self.0.to_string()   
     }
 
-    fn to_u64(&self) -> Option<u64> {
-        Some(self.0)
+    /// The function needs to find the tightest domain for the given value.
+    fn value_domain(&self) -> ValueDomain {
+        if self.0 <= std::i32::MAX && self.0 >= std::i32::MIN {
+            ValueDomain::Int
+        } else {
+            ValueDomain::Long
+        }
     }
 
-    fn to_u32(&self) -> Option<u32> {
-        // TOOD: Or should the user always call `to_u64`and cast themselves?
-        Some(self.0.try_into().ok()?)
+    fn to_i64(&self) -> i64 {
+        self.0
     }
 
-    fn serialize<Writer: DataWriter>(&self, writer: &Writer) -> Result<(), Error> {
-        writer.write_integer(self.0)
+    fn to_i32(&self) -> i32 {
+        // TODO: Maybe give a more informative error message here.
+        self.0.try_into().unwrap()
     }
 }
