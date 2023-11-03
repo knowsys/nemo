b7d1e43b95a07a6635b5d67a5b6991f024ce75b5
Larry Gonzalez <larry.gonzalez@tu-dresden.de>
2023-11-03 14:09:28 +0100

    address code review: push unsafe inside

--- nemo-physical/src/dictionary/infix_dictionary.rs
+++ nemo-physical/src/dictionary/infix_dictionary.rs
@@ -56,11 +56,9 @@ impl Dictionary for InfixDictionary {
 
     fn fetch_id(&self, string: &str) -> Option<usize> {
         if string.starts_with(self.prefix.as_str()) && string.ends_with(self.suffix.as_str()) {
-            unsafe {
-                self.dict.fetch_id(
-                    &string.get_unchecked(self.prefix.len()..string.len() - self.suffix.len()),
-                )
-            }
+            self.dict.fetch_id(unsafe {
+                &string.get_unchecked(self.prefix.len()..string.len() - self.suffix.len())
+            })
         } else {
             None
         }
