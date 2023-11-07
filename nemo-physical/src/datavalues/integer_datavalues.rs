bb0b934c7ea718fde849bb2a9d38ba1e1a984c5a
Markus Krötzsch <markus.kroetzsch@tu-dresden.de>
2023-11-07 11:03:55 +0100

    + some tests

--- nemo-physical/src/datavalues/integer_datavalues.rs
+++ nemo-physical/src/datavalues/integer_datavalues.rs
@@ -1,3 +1,7 @@
+//! This module provides implementations [`super::DataValue`]s that represent integer numbers.
+//! Integers have a more complex domain hierarchy than most other kinds of data, and the related
+//! logic is collected here as well.
+
 use super::{DataValue,ValueDomain};
 
 /// Physical representation of an integer as an i64.
@@ -45,5 +49,46 @@ impl DataValue for Long {
     fn to_i32_unchecked(&self) -> i32 {
         // TODO: Maybe give a more informative error message here.
         self.0.try_into().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Long;
+    use crate::datavalues::{DataValue,ValueDomain};
+
+
+    #[test]
+    fn test_big_long() {
+        let long_value: i64 =  <i32 as Into<i64>>::into((std::i32::MAX)) + 42;
+        let long1 = Long(long_value);
+
+        assert_eq!(long1.lexical_value(), long_value.to_string());
+        assert_eq!(long1.datatype_iri(), "http://www.w3.org/2001/XMLSchema#long".to_string());
+        assert_eq!(long1.value_domain(), ValueDomain::Long);
+
+        assert_eq!(long1.to_i64(), Some(long_value));
+        assert_eq!(long1.to_i64_unchecked(), long_value);
+        assert_eq!(long1.to_i32(), None);
+
+        assert_eq!(long1.fits_into_i32(), false);
+        assert_eq!(long1.fits_into_i64(), true);
+    }
+
+    #[test]
+    fn test_small_long() {
+        let long1 = Long(42);
+
+        assert_eq!(long1.lexical_value(), "42".to_string());
+        assert_eq!(long1.datatype_iri(), "http://www.w3.org/2001/XMLSchema#int".to_string());
+        assert_eq!(long1.value_domain(), ValueDomain::Int);
+
+        assert_eq!(long1.to_i64(), Some(42));
+        assert_eq!(long1.to_i64_unchecked(), 42);
+        assert_eq!(long1.to_i32(), Some(42));
+        assert_eq!(long1.to_i32_unchecked(), 42);
+
+        assert_eq!(long1.fits_into_i32(), true);
+        assert_eq!(long1.fits_into_i64(), true);
     }
 }
