052104f24a646af5627058242bceb8b874b6808c
Markus Krötzsch <markus.kroetzsch@tu-dresden.de>
2023-11-08 17:21:05 +0100

    +DV implementation for String domain

--- nemo-physical/src/datavalues.rs
+++ nemo-physical/src/datavalues.rs
@@ -13,4 +13,6 @@ pub use integer_datavalues::UnsignedLong;
 /// Module to define [DataValue] implementations for values that can be represented as floating point numbers.
 pub mod float_datavalues;
 pub use float_datavalues::Double;
-
+/// Module to define [DataValue] implementations for values that can be represented as Unicode strings.
+pub mod string_datavalue;
+pub use string_datavalue::StringDataValue;
---
+++ nemo-physical/src/datavalues/string_datavalue.rs
@@ -0,0 +1,45 @@
+//! This module provides implementations [`super::DataValue`]s that represent Unicode strings.
+
+use super::{DataValue,ValueDomain};
+
+/// Physical representation of a Unicode string using String.
+#[repr(transparent)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct StringDataValue(String);
+
+impl DataValue for StringDataValue {
+    fn datatype_iri(&self) -> String {
+        self.value_domain().type_iri()
+    }
+
+    fn lexical_value(&self) -> String {
+        self.0.to_owned()
+    }
+
+    fn value_domain(&self) -> ValueDomain {
+        ValueDomain::String
+    }
+
+    fn to_string_unchecked(&self) -> String {
+        self.0.to_owned()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::StringDataValue;
+    use crate::datavalues::{DataValue,ValueDomain};
+
+    #[test]
+    fn test_string() {
+        let value = "Hello world";
+        let double = StringDataValue(value.to_string());
+
+        assert_eq!(double.lexical_value(), value.to_string());
+        assert_eq!(double.datatype_iri(), "http://www.w3.org/2001/XMLSchema#string".to_string());
+        assert_eq!(double.value_domain(), ValueDomain::String);
+
+        assert_eq!(double.to_string(), Some(value.to_string()));
+        assert_eq!(double.to_string_unchecked(), value.to_string());
+    }
+}
