375da0c1ae44a184babb645fde996a81facbe875
Markus Krötzsch <markus.kroetzsch@tu-dresden.de>
2023-11-26 20:25:34 +0100

    use generic error in interface

--- nemo-physical/src/datasources/table_providers.rs
+++ nemo-physical/src/datasources/table_providers.rs
@@ -1,8 +1,9 @@
 //! Module for defining a trait that can be implemented by code that can provide tabular data,
 //! such as file readers.
 
+use std::error::Error;
+
 use super::TableWriter;
-use crate::error::ReadingError;
 
 /// TODO: this is the new TableReader, and the interface will be similar but using ColumnWriters
 pub trait TableProvider {
@@ -10,5 +11,5 @@ pub trait TableProvider {
     fn provide_table_data(
         self: Box<Self>,
         table_writer: &mut TableWriter,
-    ) -> Result<(), ReadingError>;
+    ) -> Result<(), Box<dyn Error>>;
 }
