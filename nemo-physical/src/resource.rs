e64326cd9c3a159728d22618067758e5a0bed400
Markus Krötzsch <markus.kroetzsch@tu-dresden.de>
2023-11-29 21:11:26 +0100

    record doubts about locaiotn of Resource

--- nemo-physical/src/resource.rs
+++ nemo-physical/src/resource.rs
@@ -5,4 +5,6 @@
 /// Resources are resolved using `nemo::io::resource_providers::ResourceProviders`
 ///
 /// Resources currently can be either an IRI or a (possibly relative) file path.
+/// TODO: It is not clear why this needs to be in `nemo-physical` given that it is only used and
+/// resolved by code in `nemo`.
 pub type Resource = String;
