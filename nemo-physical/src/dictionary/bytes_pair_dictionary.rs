f74e32d4571d319dded6a1179646397b0a6910a6
Larry Gonzalez <larry.gonzalez@tu-dresden.de>
2024-01-30 14:43:58 +0100

    improve documentation; add tests

--- nemo-physical/src/dictionary/bytes_pair_dictionary.rs
+++ nemo-physical/src/dictionary/bytes_pair_dictionary.rs
@@ -3,35 +3,28 @@ use super::{
     AddResult,
 };
 
-use hashbrown::{HashMap,HashSet};
-
+use hashbrown::{HashMap, HashSet};
 
 /// A struct that implements a bijection between a pair of bytes and integers.
 /// Integers are automatically assigned upon insertion.
-///
-/// TODO
-/// It assumes that the first component is unfrequent, and the second component
-/// is frequent. Two use cases are language tagged strings, and datatypes, e.g. "Deutschland"@de,
-/// and "2010539060"^^http://www.w3.org/2001/XMLSchema#integer.
 #[derive(Debug)]
 pub(crate) struct BytesPairDictionary<B: GlobalBytesBuffer> {
     set: HashSet<BytesRef<B>>,
-    map: HashMap<[BytesRef<B>;2], usize>,
-    store: Vec<[BytesRef<B>;2]>,
+    map: HashMap<[BytesRef<B>; 2], usize>,
+    store: Vec<[BytesRef<B>; 2]>,
     buffer_id: usize,
 }
 
-
 impl<B: GlobalBytesBuffer> BytesPairDictionary<B> {
-    /// Construct a new and empty string dictionary.
+    /// Construct a new and empty BytesPair dictionary.
     pub(crate) fn new() -> Self {
         Self::default()
     }
 
-    /// Adds a new string pair to the dictionary when the pair is not stored yet.
+    /// Adds a new bytes pair to the dictionary when the pair is not stored yet.
     /// Ids are assigned in consecutive order.
     ///
-    /// The result is an [AddResult] that indicates if the string pair was newly added,
+    /// The result is an [AddResult] that indicates if the bytes pair was newly added,
     /// or previoulsy present.
     pub(crate) fn add_bytes_pair(&mut self, first: &[u8], second: &[u8]) -> AddResult {
         let first_bref = self.get_or_insert_bref(first);
@@ -50,18 +43,18 @@ impl<B: GlobalBytesBuffer> BytesPairDictionary<B> {
 
     /// Looks for a given [&str] slice pair and returns `Some(id)` if the pair is in the dictionary,
     /// and `None` otherwise.
-    pub(crate) fn str_pair_to_id(&self, first: &[u8], second: &[u8]) -> Option<usize> {
+    pub(crate) fn bytes_pair_to_id(&self, first: &[u8], second: &[u8]) -> Option<usize> {
         match (self.set.get(first), self.set.get(second)) {
             (Some(fbref), Some(sbref)) => {
                 let pair = [*fbref, *sbref];
                 self.map.get(&pair).copied()
-            },
-            (_,_) => None,
+            }
+            (_, _) => None,
         }
     }
 
-    /// Returns the string pair associated with the `id`, or `None` if `id` doesn't
-    /// exists in the dictioanry.
+    /// Returns the bytes pair associated with the `id`, or `None` if `id` doesn't
+    /// exists in the dictionary.
     pub(crate) fn id_to_bytes_pair(&self, id: usize) -> Option<(Vec<u8>, Vec<u8>)> {
         match self.store.get(id) {
             Some([first_bref, second_bref]) => Some((first_bref.to_vec(), second_bref.to_vec())),
@@ -74,7 +67,7 @@ impl<B: GlobalBytesBuffer> BytesPairDictionary<B> {
         self.store.len()
     }
 
-        /// Helper function to get a BytesRef from a string using the BytesBuffer
+    /// Helper function to get a BytesRef from a array of u8 characters using the BytesBuffer
     fn get_or_insert_bref(&mut self, bytes: &[u8]) -> BytesRef<B> {
         match self.set.get(bytes) {
             Some(bref) => *bref,
@@ -87,7 +80,7 @@ impl<B: GlobalBytesBuffer> BytesPairDictionary<B> {
     }
 }
 
-impl <B: GlobalBytesBuffer>  Default for BytesPairDictionary<B> {
+impl<B: GlobalBytesBuffer> Default for BytesPairDictionary<B> {
     fn default() -> Self {
         BytesPairDictionary {
             set: HashSet::new(),
@@ -95,5 +88,36 @@ impl <B: GlobalBytesBuffer>  Default for BytesPairDictionary<B> {
             store: Vec::new(),
             buffer_id: B::init_buffer(),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::dictionary::{
+        bytes_buffer::{BytesBuffer, GlobalBytesBuffer},
+        bytes_pair_dictionary::BytesPairDictionary,
+        AddResult,
+    };
+
+    crate::dictionary::bytes_buffer::declare_bytes_buffer!(TestBytesBuffer, TEST_BUFFER);
+
+    #[test]
+    fn add_and_get() {
+        let mut dict: BytesPairDictionary<TestBytesBuffer> = BytesPairDictionary::new();
+
+        assert_eq!(
+            dict.add_bytes_pair("a".as_bytes(), "b".as_bytes()),
+            AddResult::Fresh(0)
+        );
+        assert_eq!(
+            dict.add_bytes_pair("a".as_bytes(), "b".as_bytes()),
+            AddResult::Known(0)
+        );
+
+        assert_eq!(
+            dict.bytes_pair_to_id("a".as_bytes(), "b".as_bytes()),
+            Some(0)
+        );
+        assert_eq!(dict.bytes_pair_to_id("b".as_bytes(), "a".as_bytes()), None);
     }
 }
