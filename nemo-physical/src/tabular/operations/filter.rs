a87f910bede92b5a0abb716c85eaed68068bad60
Alex Ivliev <alex.ivliev@tu-dresden.de>
2024-06-06 09:36:21 +0200

    Optimize constructors for n-ary functions

--- nemo-physical/src/tabular/operations/filter.rs
+++ nemo-physical/src/tabular/operations/filter.rs
@@ -131,11 +131,7 @@ impl GeneratorFilter {
     /// Helper function that takes a list of boolean [Filter]s
     /// and constructs a [Filter] that represent its conjuction.
     fn fold_filters(filters: Vec<&Filter>) -> Filter {
-        if filters.len() == 1 {
-            filters[0].clone()
-        } else {
-            Filter::boolean_conjunction(filters.into_iter().cloned().collect::<Vec<_>>())
-        }
+        Filter::boolean_conjunction(filters.into_iter().cloned().collect::<Vec<_>>())
     }
 
     /// Compute the [FilterAssignment] from a list of [Filters].
