f1a68054b37c7e4292c32d20568197ad8b0b015e
Jakob Steinberg <jakob_maximilian.steinberg@mailbox.tu-dresden.de>
2024-07-30 12:07:44 +0200

    Use syntax constants instead of str values

--- nemo-physical/src/datavalues/syntax.rs
+++ nemo-physical/src/datavalues/syntax.rs
@@ -2,20 +2,52 @@
 //! These are kept in one location, since they are required in various
 //! places related to parsing and display.
 
-/// Opening delimiter for tuples.
-pub const DELIM_TUPLE_OPEN: &str = "(";
-/// Closing delimiter for tuples.
-pub const DELIM_TUPLE_CLOSE: &str = ")";
-/// Opening delimiter for tuples.
-pub const DELIM_MAP_OPEN: &str = "{";
-/// Closing delimiter for tuples.
-pub const DELIM_MAP_CLOSE: &str = "}";
-/// Assignment operator for maps.
-pub const MAP_ASSIGN: &str = "=";
-/// Separator for key-value pairs in maps
-pub const MAP_SEPARATOR: &str = ",";
-/// Separator for elements of tuples
-pub const TUPLE_SEPARATOR: &str = ",";
+/// This module defines the syntax for tuple values
+pub mod tuple {
+    /// Opening delimiter for tuples.
+    pub const OPEN: &str = "(";
+    /// Closing delimiter for tuples.
+    pub const CLOSE: &str = ")";
+    /// Separator for elements of tuples
+    pub const SEPARATOR: &str = ",";
+}
+
+/// This module defines the syntax for map values
+pub mod map {
+    /// Opening delimiter for tuples.
+    pub const OPEN: &str = "{";
+    /// Closing delimiter for tuples.
+    pub const CLOSE: &str = "}";
+    /// Assignment operator for maps.
+    pub const KEY_VALUE_ASSIGN: &str = ":";
+    /// Separator for key-value pairs in maps
+    pub const SEPARATOR: &str = ",";
+}
+
+/// This module defines the boolean values
+pub mod boolean {
+    /// True
+    pub const TRUE: &str = "true";
+    /// False
+    pub const FALSE: &str = "false";
+}
+
+/// This module defines the syntax for IRIs
+pub mod iri {
+    /// Opening delimiter for IRIs
+    pub const OPEN: &str = "<";
+    /// Closing delimiter for IRIs
+    pub const CLOSE: &str = ">";
+}
+
+/// This module defines the syntax for strings
+pub mod string {
+    /// Language tag indicator after strings
+    pub const LANG_TAG: &str = "@";
+}
+
+/// RDF datatype indicator
+pub const RDF_DATATYPE_INDICATOR: &str = "^^";
 
 /// Initial part of IRI in all XML Schema types:
 pub const XSD_PREFIX: &str = "http://www.w3.org/2001/XMLSchema#";
