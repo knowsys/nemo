f73c77c0618508b49afce0873e752a6baafe4be0
Alex Ivliev <alex.ivliev@tu-dresden.de>
2025-07-23 05:43:27 +0200

    add experiment setup

--- nemo-cli/src/cli.rs
+++ nemo-cli/src/cli.rs
@@ -159,6 +159,8 @@ impl OutputArgs {
 pub(crate) struct ExperimentArgs {
     #[arg(long = "x-create-queries")]
     pub create_queries: bool,
+    #[arg(logn = "x-trace-node-query")]
+    pub trace_node_query: Option<PathBuf>,
 }
 
 /// Cli arguments related to tracing
