4e4695bdd038928e08cdd83530aa11daf3945afa
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-08-13 19:37:21 +0200

    WIP: Support serialising JSON as AnyDataValue

--- nemo/src/io/formats/json/datavalues/map.rs
+++ nemo/src/io/formats/json/datavalues/map.rs
@@ -1,21 +1,35 @@
 pub(crate) mod default {
-    use nemo_physical::datavalues::{DataValue, MapDataValue};
-    use serde::{de::Visitor, Deserializer, Serializer};
 
+    use nemo_physical::datavalues::{DataValue, MapDataValue};
+    use serde::{de::Visitor, ser::SerializeMap, Deserializer, Serializer};
+
+    type AnyWrapper = crate::io::formats::json::variants::default::JsonAnyDataValue;
     struct MapVisitor {}
 
     impl<'de> Visitor<'de> for MapVisitor {
         type Value = MapDataValue;
 
         fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            write!(formatter, "a boolean")
+            write!(formatter, "a map")
         }
 
-        fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E>
+        fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
         where
-            E: serde::de::Error,
+            A: serde::de::MapAccess<'de>,
         {
-            Ok(MapDataValue::new(v))
+            let mut entries =
+                Vec::<(AnyWrapper, AnyWrapper)>::with_capacity(map.size_hint().unwrap_or_default());
+
+            while let Some(entry) = map.next_entry()? {
+                entries.push(entry)
+            }
+
+            Ok(MapDataValue::new(
+                None,
+                entries
+                    .into_iter()
+                    .map(|(key, value)| (key.into(), value.into())),
+            ))
         }
     }
 
@@ -23,13 +37,26 @@ pub(crate) mod default {
     where
         D: Deserializer<'de>,
     {
-        deserializer.deserialize_string(MapVisitor {})
+        deserializer.deserialize_map(MapVisitor {})
     }
 
     pub(crate) fn serialize<S>(value: &MapDataValue, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        serializer.serialize_bool(value.to_boolean_unchecked())
+        let mut map = serializer.serialize_map(value.length())?;
+
+        assert!(value.label().is_none(), "cannot serialize maps with labels");
+
+        if let Some(entries) = value.map_items() {
+            for (key, value) in entries {
+                map.serialize_entry(
+                    &AnyWrapper::from(key.clone()),
+                    &AnyWrapper::from(value.clone()),
+                )?;
+            }
+        }
+
+        map.end()
     }
 }
--- nemo/src/io/formats/json/datavalues/tuple.rs
+++ nemo/src/io/formats/json/datavalues/tuple.rs
@@ -1,21 +1,31 @@
 pub(crate) mod default {
     use nemo_physical::datavalues::{DataValue, TupleDataValue};
-    use serde::{de::Visitor, Deserializer, Serializer};
+    use serde::{de::Visitor, ser::SerializeSeq, Deserializer, Serializer};
 
+    type AnyWrapper = crate::io::formats::json::variants::default::JsonAnyDataValue;
     struct TupleVisitor {}
 
     impl<'de> Visitor<'de> for TupleVisitor {
         type Value = TupleDataValue;
 
         fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            write!(formatter, "a boolean")
+            write!(formatter, "a list")
         }
 
-        fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E>
+        fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
         where
-            E: serde::de::Error,
+            A: serde::de::SeqAccess<'de>,
         {
-            Ok(TupleDataValue::new(v))
+            let mut entries = Vec::<AnyWrapper>::with_capacity(seq.size_hint().unwrap_or_default());
+
+            while let Some(entry) = seq.next_element()? {
+                entries.push(entry);
+            }
+
+            Ok(TupleDataValue::new(
+                None,
+                entries.into_iter().map(|entry| entry.into()),
+            ))
         }
     }
 
@@ -23,13 +33,26 @@ pub(crate) mod default {
     where
         D: Deserializer<'de>,
     {
-        deserializer.deserialize_string(TupleVisitor {})
+        deserializer.deserialize_seq(TupleVisitor {})
     }
 
     pub(crate) fn serialize<S>(value: &TupleDataValue, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        serializer.serialize_bool(value.to_boolean_unchecked())
+        assert!(
+            value.label().is_none(),
+            "cannot serialize tuples with labels"
+        );
+        let length = value.length().expect("tuples always have a fixed length");
+        let mut seq = serializer.serialize_seq(Some(length))?;
+
+        for idx in 0..length {
+            seq.serialize_element(&AnyWrapper::from(
+                value.tuple_element_unchecked(idx).clone(),
+            ))?;
+        }
+
+        seq.end()
     }
 }
