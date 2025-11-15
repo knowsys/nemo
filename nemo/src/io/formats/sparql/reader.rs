5c2ec1caf0bf82cb3b02039b3d95b2247ce9ba5b
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-11-15 14:25:07 +0100

    Also start import timing instance for SPARQL queries

--- nemo/src/io/formats/sparql/reader.rs
+++ nemo/src/io/formats/sparql/reader.rs
@@ -73,6 +73,9 @@ impl SparqlReader {
         tuple_writer: &mut TupleWriter<'_>,
     ) -> Result<(), ReadingError> {
         TimedCode::instance()
+            .sub("Reasoning/Execution/Import")
+            .start();
+        TimedCode::instance()
             .sub("Reasoning/Execution/Import/SPARQL queries")
             .start();
 
@@ -82,6 +85,9 @@ impl SparqlReader {
             .expect("query result should not be empty");
         TimedCode::instance()
             .sub("Reasoning/Execution/Import/SPARQL queries")
+            .stop();
+        TimedCode::instance()
+            .sub("Reasoning/Execution/Import")
             .stop();
         Self::read_table_data(
             response,
