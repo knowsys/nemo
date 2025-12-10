9c5cecfad544f594d811812b23ec383707b12df4
Alex Ivliev <alex.ivliev@tu-dresden.de>
2025-12-10 18:12:06 +0100

    Marking of import predicates if merging optimization is disabled

--- nemo/src/execution/planning/operations/join_cartesian.rs
+++ nemo/src/execution/planning/operations/join_cartesian.rs
@@ -49,7 +49,7 @@ impl GeneratorJoinCartesian {
         operations: &mut Vec<Operation>,
         atoms_negation: &mut Vec<BodyAtom>,
     ) -> Self {
-	log::info!("not cartesianing");
+        log::info!("not cartesianing");
         let join = GeneratorJoinSeminaive::new(atoms.to_vec(), order);
         let filter = GeneratorFunctionFilterNegation::new(
             join.output_variables(),
@@ -71,7 +71,7 @@ impl GeneratorJoinCartesian {
         operations: &mut Vec<Operation>,
         atoms_negation: &mut Vec<BodyAtom>,
     ) -> Self {
-	log::info!("cartesianing");
+        log::info!("cartesianing");
         let mut old_operations = Vec::default();
         let mut old_negations = Vec::default();
 
